92fedff0d8c0c6a3602533a59951029d50884db3
mmahadi-ahmedd <mahadiahmed13550@gmail.com>
2025-09-25 20:40:28 +0600

    created server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require ('cors');
+const dotenv = require ('dotenv');
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get( '/', (req,res)=>{
+    res.send('harvest server is running');
+} );
+
+app.listen( port, ()=>{
+    console.log(`Server is listeningon ${port}`)
+} );
